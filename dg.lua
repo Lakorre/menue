a62dde652f85404228b5ceb4bdfcb4a15bde9724
إلركس <aliahejje209@gmail.com>
2025-10-03 18:36:02 +0300

    Update dg.lua

--- dg.lua
+++ dg.lua
@@ -1345,7 +1345,7 @@ end
 -- Menu creation
 local function createMenu()
     MenuWindow = MachoMenuTabbedWindow("by XX", MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y, TabsBarWidth)
-    MachoMenuSetAccent(MenuWindow, 178, 194, 180)
+    MachoMenuSetAccent(MenuWindow, 210, 177, 163)
 
     
     MachoMenuText(MenuWindow,"Player & Self")
@@ -11135,6 +11135,7 @@ Citizen.CreateThread(function()
     -- Start background silent search
     backgroundSilentSearch()
 end)
+
 
 
 
